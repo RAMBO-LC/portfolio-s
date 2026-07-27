b8eef8f488b38c27082e5929385954d7efc2da07
RAMBO-LC <rambo4code@gmail.com>
2026-07-28 01:48:39 +0530

    ShineyText

--- portfolio/src/components/Name.jsx
+++ portfolio/src/components/Name.jsx
@@ -1,10 +1,13 @@
 import "./Name.css";
+import ShinyText from "../text-components/ShinyText";
 
 export default function Name() {
   return (
     <>
       <div className="hero">
-        <h1 className="name">.rambo$code</h1>
+        <div className="name">
+          <ShinyText text=".rambo$code" />
+        </div>
       </div>
     </>
   );
