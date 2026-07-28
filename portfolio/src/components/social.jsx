c109c22c57c5db188d263fcf3c0e1fcbbba28d6b
RAMBO-LC <rambo4code@gmail.com>
2026-07-28 23:42:04 +0530

    social bar improved

--- portfolio/src/components/social.jsx
+++ portfolio/src/components/social.jsx
@@ -1,6 +1,6 @@
 import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-import "./social.css";
+import "./Social.css";
 export default function Social() {
   return (
     <>
