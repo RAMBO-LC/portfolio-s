47051b0d22cdd0024a3571c7b5fd88e7d9f2adfa
RAMBO-LC <rambo4code@gmail.com>
2026-07-29 11:11:43 +0530

    social bar improved; _blank

--- portfolio/src/components/Social.jsx
+++ portfolio/src/components/Social.jsx
@@ -1,21 +1,27 @@
-import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaDiscord, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import "./Social.css";
 export default function Social() {
   return (
     <>
       <div className="social-box">
-        <a href="https://github.com/RAMBO-LC/">
+        <a href="https://github.com/RAMBO-LC/" target="_blank">
           <FaGithub />
         </a>
-        <a href="https://www.linkedin.com/in/raj-ambolikar-9734b4326/">
+        <a
+          href="https://www.linkedin.com/in/raj-ambolikar-9734b4326/"
+          target="_blank"
+        >
           <FaLinkedin />
         </a>
-        <a href="https://x.com/CodeRambo15238">
+        <a href="https://x.com/CodeRambo15238" target="_blank">
           <FaXTwitter />
         </a>
-        <a href="https://www.instagram.com/rambo.1.07/">
+        <a href="https://www.instagram.com/rambo.1.07/" target="_blank">
           <FaInstagram />
+        </a>
+        <a href="https://discord.com/users/1203988845633347639" target="_blank">
+          <FaDiscord />
         </a>
       </div>
     </>
