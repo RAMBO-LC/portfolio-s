bcdef225f7ac08392b6f759d6b43c2f0dc6da7c7
RAMBO-LC <rambo4code@gmail.com>
2026-07-30 11:43:53 +0530

    Update App.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.css";
 import Home from "./Pages/Home.jsx";
-import About from "./Pages/About.jsx";
 
 function App() {
   return (
     <>
       <Home />
-      <About />
+
     </>
   );
 }
